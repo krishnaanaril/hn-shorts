7f8c9b450d418afb895e500ebd4a8257c1583054
krishnaanaril <krishnamohan.a.m@gmail.com>
2022-10-29 20:32:10 +0530

    feat: add share feature

--- projects/hn-shorts/src/app/components/story/story.component.ts
+++ projects/hn-shorts/src/app/components/story/story.component.ts
@@ -4,7 +4,9 @@ import { saveAs } from 'file-saver';
 import { toBlob } from 'html-to-image';
 
 import { Story } from '../../shared/models/story';
+import { ShareData } from '../../shared/models/share-data'
 import { HnDataService } from '../../shared/services/hn-data.service';
+import { NotificationService } from '../../shared/services/notification.service';
 
 @Component({
   selector: 'app-story',
@@ -14,11 +16,12 @@ import { HnDataService } from '../../shared/services/hn-data.service';
 export class StoryComponent implements OnInit {
 
   @Input() storyId!: number;
-  public storyDetails!: Story | null;
+  public storyDetails!: Story | null;  
 
   constructor(
     @Inject(DOCUMENT)
     private document: Document,
+    private notificationService: NotificationService,
     private dataService: HnDataService
   ) { }
 
@@ -43,7 +46,9 @@ export class StoryComponent implements OnInit {
       case 'Comment':
         this.viewComments();
         break;
-      case 'Share': break;
+      case 'Share': 
+      this.shareStory();
+       break;
       case 'Save': 
         this.downloadImage();
         break;
@@ -51,14 +56,37 @@ export class StoryComponent implements OnInit {
   }
 
   viewStory() {
-    if (!this.storyDetails?.url)
-      this.viewComments();
-    window.open(this.storyDetails?.url, "_blank");
+    const storyUrl = this.getStoryUrl();
+    window.open(storyUrl, "_blank");
+  }
+
+  async shareStory() {
+    const shareData = {
+      url: this.getStoryUrl(),
+      title: "Hacker News | Shorts",
+      text: this.storyDetails?.title
+    } as ShareData;
+    if(!navigator.canShare){
+      console.log("No support for share");
+      this.notificationService.open('Sorry, Your browser do not support this feature');
+    } 
+    else {
+      try {
+        await navigator.share(shareData);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+
+  getStoryUrl() {
+    const commentUrl = `https://news.ycombinator.com/item?id=${this.storyDetails?.id}`;
+    return this.storyDetails?.url ?? commentUrl;
   }
 
   viewComments() {
-    const storyUrl = `https://news.ycombinator.com/item?id=${this.storyDetails?.id}`;
-    window.open(storyUrl, "_blank");
+    const commentUrl = `https://news.ycombinator.com/item?id=${this.storyDetails?.id}`;
+    window.open(commentUrl, "_blank");
   }
 
   downloadImage() {
