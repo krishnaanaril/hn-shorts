833b94618d083a8c6beaf2289bfd16542155149f
krishnaanaril <krishnamohan.a.m@gmail.com>
2022-11-14 11:08:24 +0530

    minor: remove progress bar module

--- projects/hn-shorts/src/app/material.module.ts
+++ projects/hn-shorts/src/app/material.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
@@ -10,8 +9,7 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
   exports: [
     MatButtonModule,
     MatButtonToggleModule,
-    MatIconModule,
-    MatProgressBarModule,
+    MatIconModule,    
     MatSidenavModule,
     MatSnackBarModule
   ]
